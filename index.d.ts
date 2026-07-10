c22bb178f0980606996717ecb3f393bff1faaa36
LeonPuchinger <lepu1590@gmail.com>
2026-07-10 15:50:30 +0200

    Expose type information from the library

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+declare class SynchronousSocket {
+  constructor(socketPath: string);
+  connect(): void;
+  disconnect(): void;
+  read(limit?: number): string | null;
+  readIntoBuffer(buffer: ArrayBufferView | Uint8Array): number | null;
+  write(data: string): void;
+  writeFromBuffer(buffer: ArrayBufferView | Uint8Array): number;
+}
+
+declare class SynchronousSocketServer {
+  constructor(socketPath: string);
+  listen(backlog?: number): void;
+  accept(): SynchronousSocket;
+  close(): void;
+}
+
+declare const _default: {
+  SynchronousSocket: typeof SynchronousSocket;
+  SynchronousSocketServer: typeof SynchronousSocketServer;
+};
+
+export = _default;
