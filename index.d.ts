80d04063d9b44d7a1994cdf09848008e3c264fce
Leon Puchinger <24792398+LeonPuchinger@users.noreply.github.com>
2026-07-14 10:09:50 +0200

    Allow instantiation of a server instance (#2)

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export declare class SynchronousSocket {
+  constructor(socketPath: string);
+  connect(): void;
+  disconnect(): void;
+  read(limit?: number): string | null;
+  readIntoBuffer(buffer: ArrayBufferView | Uint8Array): number | null;
+  write(data: string): void;
+  writeFromBuffer(buffer: ArrayBufferView | Uint8Array): number;
+}
+
+export declare class SynchronousSocketServer {
+  constructor(socketPath: string);
+  listen(backlog?: number): void;
+  accept(): SynchronousSocket;
+  close(): void;
+}
